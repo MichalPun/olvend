812620a872d2ffa117422a32cd7d31214a3f13a3
Michal Punčochář <michalpuncochar@Michal--MacBook-Air.local>
2026-04-25 12:57:17 +0200

    Add bagette recurring order reset script

---
+++ database/reset_bagette_purchase_profiles_v13.sql
@@ -0,0 +1,16 @@
+begin;
+
+delete from public.purchase_order_items
+where purchase_order_id in (
+  select id
+  from public.purchase_orders
+  where order_scope = 'bagette_pilot'
+);
+
+delete from public.purchase_orders
+where order_scope = 'bagette_pilot';
+
+delete from public.purchase_product_profiles
+where pilot_scope = 'bagette';
+
+commit;
